3b21787ba6821f1d2c8f2422304acdba5cc00cef
kcsamm <kcsamm11@gmail.com>
2021-06-12 19:28:52 +0100

    Show individual post functionality (via passing post id in url)

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -2,17 +2,23 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-            <!-- Iterate though posts after checkong if any exists -->
-            @if($posts->count())
-                @foreach ($posts as $post)
-                    <x-post :post="$post" />
-                @endforeach
+        <div class="w-8/12">
+            <div class="p-6">
+                <h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
+                <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }} and recieved {{ $user->receivedLikes->count() }} likes</p>
+            </div>
+            <div class="bg-white p-6 rounded-lg">
+                <!-- Iterate though posts after checkong if any exists -->
+                @if($posts->count())
+                    @foreach ($posts as $post)
+                        <x-post :post="$post" />
+                    @endforeach
 
-                {{ $posts->links() }}
-            @else
-                <p>{{ $user->name }} does not have any posts</p>
-            @endif
+                    {{ $posts->links() }}
+                @else
+                    <p>{{ $user->name }} does not have any posts</p>
+                @endif
+            </div>
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,6 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 
 Route::get('/users/{user:username}/posts', [UserPostController::class, 'index'])->name('users.posts');
 
-
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
@@ -39,6 +38,7 @@ Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts', [PostController::class, 'store']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
